085b85a8b374c5fe9dd1e6532e32fbf72e622ca8
caringaljohnlloyd <130682372+caringaljohnlloyd@users.noreply.github.com>
2023-10-01 23:22:17 +0800

    .

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -8,28 +8,27 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->match(['get', 'post'], '/signup', 'Home::register');
-$routes->match(['get', 'post'], '/signin', 'Home::LoginAuth');
+$routes->post('/signin', 'UserController::LoginAuth');
 
 
-$routes->get('/user', 'Home::home');
+$routes->get('/user', 'Home::home',['filter'=>'authGuard']);
 
-$routes->get('/shop', 'Home::shop');
-$routes->get('/about', 'Home::about');
-$routes->get('/blog', 'Home::blog');
-$routes->get('/contact', 'Home::contact');
+$routes->get('/shop', 'Home::shop',['filter'=>'authGuard']);
+$routes->get('/about', 'Home::about',['filter'=>'authGuard']);
+$routes->get('/blog', 'Home::blog',['filter'=>'authGuard']);
+$routes->get('/contact', 'Home::contact',['filter'=>'authGuard']);
 
-$routes->get('/sidebar', 'AdminController::admin');
-$routes->get('/adminchart/chart', 'AdminController::adminch');
-$routes->get('/admintable/table', 'AdminController::admintab');
+$routes->get('/sidebar', 'AdminController::admin',['filter'=>'authGuard']);
+$routes->get('/adminchart/chart', 'AdminController::adminch',['filter'=>'authGuard']);
+$routes->get('/admintable/table', 'AdminController::admintab',['filter'=>'authGuard']);
 
 
-$routes->get('/admininventory/inventory', 'AdminController::inventory');
+$routes->get('/admininventory/inventory', 'AdminController::inventory',['filter'=>'authGuard']);
 
-$routes->post('/admininventory/add', 'AdminController::add');
+$routes->post('/admininventory/add', 'AdminController::add',['filter'=>'authGuard']);
 $routes->post('/admininventory/update/(:num)', 'AdminController::edit/$1');
 $routes->get('/admininventory/delete/(:num)', 'AdminController::delete/$1');
 
-['filter'=>'authGuard'];
 
 
 
